2585c2943f61ca3e6d9dfae02baa098a611242bd
Gabriel Konkle <gabriel.konkle@gmail.com>
2025-02-26 09:47:12 -0700

    Update go package to reflect types

--- packages/go/main.go
+++ packages/go/main.go
@@ -1,92 +1,85 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
-	// "github.com/swaggest/jsonschema-go"
+
+	"github.com/swaggest/jsonschema-go"
 )
 
-type Route[I any, O any] struct {
-	Handler func(I) O
+type Route struct {
+	Handler func(*http.Request) any
 	// Authorizer func(req *http.Request, input I) bool
 }
 
-type Routes map[string]any
-
 type SomnolenceServer struct {
-	Port int
 	Start func()
 }
 
-func CreateSomnolenceServer(port int, routes Routes) SomnolenceServer {
-	// reflector := jsonschema.Reflector{}
+func CreateSomnolenceServer[T any](port int, routes map[string]Route) SomnolenceServer {
 	somnolenceServer := SomnolenceServer{
-		Port: port,
-	}
-	somnolenceServer.Start = func() {
-		fmt.Printf("💤 Somnolence is running at http://localhost:%v\n", port)
-
-		for path, route := range routes {
-			typedRoute := route.(Route[any, any])
-			http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
-				// if path == "/__schema" {
-				// 	input := reflector.New(route.Input)
-				// 	err := json.NewDecoder(r.Body).Decode(input)
-				// 	if err != nil {
-				// 		http.Error(w, err.Error(), http.StatusBadRequest)
-				// 		return
-				// 	}
-
-				// 	if route.Authorizer != nil && !route.Authorizer(r, input) {
-				// 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
-				// 		return
-				// 	}
-
-				// 	output := reflector.New(route.Output)
-				// 	output = route.Handler(input)
-
-				// 	json.NewEncoder(w).Encode(output)
-				// } else {
-				fmt.Fprintf(w, "%s", typedRoute.Handler(r.URL.Query()))
-				// }
+		Start: func() {
+			// Handle __schema route
+			http.HandleFunc("/__schema", func(w http.ResponseWriter, req *http.Request) {
+				reflector := jsonschema.Reflector{}
+				var t T
+				schema, err := reflector.Reflect(t)
+				if err != nil {
+					log.Fatal(err)
+				}
+				j, err := json.MarshalIndent(schema, "", " ")
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Fprintf(w, "%s", string(j))
 			})
-		}
-
-		err := http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
-		if errors.Is(err, http.ErrServerClosed) {
-			fmt.Println("server closed")
-		} else if err != nil {
-			fmt.Printf("error starting server: %s\n", err)
-			os.Exit(1)
-		}
+	
+			// Handle user-defined routes
+			for path, route := range routes {
+				http.HandleFunc(path, func(w http.ResponseWriter, req *http.Request) {
+					fmt.Fprintf(w, "%s", route.Handler(req))
+				})
+			}
+	
+			// Start server
+			fmt.Printf("💤 Somnolence is running at http://localhost:%v\n", port)
+			err := http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
+			if errors.Is(err, http.ErrServerClosed) {
+				fmt.Println("server closed")
+			} else if err != nil {
+				fmt.Printf("error starting server: %s\n", err)
+				os.Exit(1)
+			}
+		},
 	}
 	return somnolenceServer
 }
 
-func CreateRoute[I any, O any](route Route[I, O]) Route[I, O]{
-	return Route[I, O]{
-		Handler: route.Handler,
-		// Authorizer: route.Authorizer,
-	}
-}
+
 
 func main() {
-	somnolenceServer := CreateSomnolenceServer(3000, Routes{
-		"/hello": CreateRoute(Route[
-			struct {
-				name string
+	type ExampleRoutes struct {
+		Hello struct {
+			Input struct {
+				Name string `json:"name"`
+			} `json:"input"`
+			Output struct {
+				Message string `json:"message"`
+			} `json:"output"`
+		} `json:"/hello"`
+	}
+	somnolenceServer := CreateSomnolenceServer[ExampleRoutes](3000, map[string]Route{
+		"/hello": {
+			Handler: func(req *http.Request) any {
+				return struct {message string}{
+					message: fmt.Sprintf("Hello, %s!", req.URL.Query().Get("name")),
+				}
 			},
-			struct {
-				message string
-			},
-		]{
-			Handler: func(input struct {name string}) struct {message string} {
-			return struct {message string}{
-				message: fmt.Sprintf("Hello, %s!", input.name),
-			}
-		}}),
+		},
 	})
 	somnolenceServer.Start()
 }
