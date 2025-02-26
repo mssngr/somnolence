b55f41f9f331fb89a2dfc35af389270b0f2ad4f3
Gabriel Konkle <gabriel.konkle@gmail.com>
2025-02-26 02:05:29 -0700

    Getting further into Go implementation

--- packages/go/go.mod
+++ packages/go/go.mod
@@ -1,3 +1,7 @@
 module github.com/mssngr/somnolence/tree/main/packages/go
 
 go 1.24.0
+
+require github.com/swaggest/jsonschema-go v0.3.73
+
+require github.com/swaggest/refl v1.3.0 // indirect
--- packages/go/main.go
+++ packages/go/main.go
@@ -5,34 +5,62 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	// "github.com/swaggest/jsonschema-go"
 )
 
-type Route struct {
-	Input any
-	Output any
-	Handler func(input any) any
-	Authorizer func(req any, input any) bool
+type Route[I any, O any] struct {
+	Handler func(I) O
+	// Authorizer func(req *http.Request, input I) bool
 }
+
+type Routes map[string]any
 
 type SomnolenceServer struct {
 	Port int
 	Start func()
 }
 
-func CreateSomnolenceServer(port int, routes []Route) SomnolenceServer {
+func CreateSomnolenceServer(port int, routes Routes) SomnolenceServer {
+	// reflector := jsonschema.Reflector{}
 	somnolenceServer := SomnolenceServer{
 		Port: port,
 	}
 	somnolenceServer.Start = func() {
-		fmt.Printf("💤 Somnolence is running at http://localhost:%v", port)
+		fmt.Printf("💤 Somnolence is running at http://localhost:%v\n", port)
+
+		for path, route := range routes {
+			typedRoute := route.(Route[any, any])
+			http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+				// if path == "/__schema" {
+				// 	input := reflector.New(route.Input)
+				// 	err := json.NewDecoder(r.Body).Decode(input)
+				// 	if err != nil {
+				// 		http.Error(w, err.Error(), http.StatusBadRequest)
+				// 		return
+				// 	}
+
+				// 	if route.Authorizer != nil && !route.Authorizer(r, input) {
+				// 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				// 		return
+				// 	}
+
+				// 	output := reflector.New(route.Output)
+				// 	output = route.Handler(input)
+
+				// 	json.NewEncoder(w).Encode(output)
+				// } else {
+				fmt.Fprintf(w, "%s", typedRoute.Handler(r.URL.Query()))
+				// }
+			})
+		}
 
 		http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Query().Get("name"))
 		})
 
-		err := http.ListenAndServe(":3333", nil)
+		err := http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
 		if errors.Is(err, http.ErrServerClosed) {
-			fmt.Printf("server closed\n")
+			fmt.Println("server closed")
 		} else if err != nil {
 			fmt.Printf("error starting server: %s\n", err)
 			os.Exit(1)
@@ -41,7 +69,28 @@ func CreateSomnolenceServer(port int, routes []Route) SomnolenceServer {
 	return somnolenceServer
 }
 
+func CreateRoute[I any, O any](route Route[I, O]) Route[I, O]{
+	return Route[I, O]{
+		Handler: route.Handler,
+		// Authorizer: route.Authorizer,
+	}
+}
+
 func main() {
-	somnolenceServer := CreateSomnolenceServer(8080, []Route{})
+	somnolenceServer := CreateSomnolenceServer(3000, Routes{
+		"/hello": CreateRoute(Route[
+			struct {
+				name string
+			},
+			struct {
+				message string
+			},
+		]{
+			Handler: func(input struct {name string}) struct {message string} {
+			return struct {message string}{
+				message: fmt.Sprintf("Hello, %s!", input.name),
+			}
+		}}),
+	})
 	somnolenceServer.Start()
 }
