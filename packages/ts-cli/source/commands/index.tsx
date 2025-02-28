fddb72eb9274a249d2360f4d4879a1a272316cb0
Gabriel Konkle <gabriel.konkle@gmail.com>
2025-02-28 04:31:50 -0700

    CLI generating typescript types

--- packages/ts-cli/source/commands/index.tsx
+++ packages/ts-cli/source/commands/index.tsx
@@ -1,4 +1,6 @@
 import { Text } from 'ink'
+import { compile } from 'json-schema-to-typescript'
+import type { JSONSchema } from 'json-schema-to-typescript/dist/src/types/JSONSchema.js'
 import React from 'react'
 import zod from 'zod'
 
@@ -10,7 +12,26 @@ type Props = {
   options: zod.infer<typeof options>
 }
 
+function capitalizeFirstLetter(str: string) {
+  return String(str).charAt(0).toUpperCase() + String(str).slice(1)
+}
+
 export default function Index({ options }: Props) {
+  fetch(`${options.name}/__schema`).then(response =>
+    response.json().then(routes => {
+      Object.entries(routes).forEach(([path, route]) => {
+        console.log(route)
+        compile(
+          { properties: route as JSONSchema },
+          capitalizeFirstLetter(path),
+          { additionalProperties: false, bannerComment: '' },
+        ).then(ts => {
+          console.log(ts)
+        })
+      })
+    }),
+  )
+
   return (
     <Text>
       Hello, <Text color="green">{options.name}</Text>
